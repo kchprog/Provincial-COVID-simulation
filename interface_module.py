4eac8ab9bdce38d01d2889657f22ce2accf810bd
kchprog <86860267+kchprog@users.noreply.github.com>
2021-12-10 04:06:17 -0500

    Created interface module

--- interface_module.py
+++ interface_module.py
@@ -1,6 +1,6 @@
 # the interface module for the CSC110 Final project acts as a 'management system'
-
 import math_module as mSim
+from math_module import Sector as Sector
 import datetime as dt
 # import map_module
 
@@ -20,6 +20,39 @@ class math_module_processor:
         for i in range(epochs):
             self.simulation_state_dict[i] = self.sim_system.update_global_simulation()
         return self.simulation_state_dict
+
+
+class graphable_sector:
+    s_proportion = 0
+    i_proportion = 0
+    r_proportion = 0
+    v_proportion = 0
+    sector_type = ""
+    
+    def __init__(self, sector: Sector):
+        self.s_proportion = sector.susceptible_proportion
+        self.i_proportion = sector.infectious_proportion
+        self.r_proportion = sector.recovered_proportion
+        self.v_proportion = sector.vaccinated_proportion
+        self.sector_type = sector.type
+
+
+def convert_sector_info_to_mappable_information(input_dictionary: dict) -> dict:
+    # variables
+    dict_to_return = {}
+    
+    for key in input_dictionary:
+        dict_to_return[key] = graphable_sector(input_dictionary[key])
+        
+    return dict_to_return
+
+
+def graph_results(input_dictionary: dict):
+    """
+    Takes in a dictionary of the simulated results as graphical_sector objects
+    and graphs them using the map_module imported.
+    """
+    return None
 
 
 def main():
