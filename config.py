c62d0187fc27e919df2d7f26a4b5a5ad8c838b0b
kchprog <86860267+kchprog@users.noreply.github.com>
2021-12-11 22:54:37 -0500

    new parameter

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # effectiveness factor of recovered status and vaccination status
 
-vaccinated_vulnerability = 0.1
+vaccinated_vulnerability = 0.15
 recovered_vulnerability = 0.2
 
 # the average number of people a single infected individual infects over the course of the infectious period.
@@ -13,4 +13,5 @@ global_recovery_rate = 1.0/14.0
 daily_infection_rate = global_r0 / 14.0
 
 # rate at which the recovered proportion of a province becomes susceptible again
-global_recovery_fall_rate = 0.02
+global_recovery_fall_rate = 0.04
+global_vaccination_fall_rate = 0.03
