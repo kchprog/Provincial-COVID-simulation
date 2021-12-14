2e6c26cb3a11fe835c3a218391929615dc386a31
kchprog <86860267+kchprog@users.noreply.github.com>
2021-12-13 21:32:30 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
-# effectiveness factor of recovered status and vaccination status
+# effectiveness factor of recovered status and vaccination status. These are derived from the Government
+# of Canada's "Health and Human Services Canada" website.
 
-vaccinated_vulnerability = 0.
-recovered_vulnerability = 0.1
+vaccinated_vulnerability = 0.05
+recovered_vulnerability = 0.15
 
 # the average number of people a single infected individual infects over the course of the infectious period.
 # in each sector, we count this factor as being influenced by density and interaction probability; it is not a static value.
 global_r0 = 2.1
 
-# how quickly an infected individual recovers from the infection, becoming noninfectious
-global_recovery_rate = 1.0/7.0
+# how quickly an infected individual recovers from the infection, becoming noninfectious. These are
+# derived from the CDC source
+global_recovery_rate = 1.0/11.0
 
-daily_infection_rate = global_r0 / 28.0
+daily_infection_rate = global_r0 / 11.0
 
 # rate at which the recovered proportion of a province becomes susceptible again
 global_recovery_fall_rate = 0.01
 global_vaccination_fall_rate = 0.005
 
-#global vaccination rollout rate
+#global vaccination rollout rate; This is an arbitrary constant.
 global_vaccination_rollout_rate = 0.3
+
